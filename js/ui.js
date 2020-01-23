901439b15c1c604135012369724512c2f9307e2d
Grant Keller <grant@kellerdigital.com>
2020-01-22 22:09:18 -0800

    Added app close button

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,10 @@
+const remote = require('electron').remote;
+
+document.getElementById("close-btn").addEventListener("click", function (e) {
+    var window = remote.getCurrentWindow();
+    window.close();
+});
+
 document.querySelector('.tab').addEventListener('click', function() {
     document.querySelector('.settings').classList.toggle('open')
 })
